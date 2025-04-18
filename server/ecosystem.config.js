fc4fe27d8d8fd4085885b7fc940ba470a536a9cf
chenzhibin <chenzb961@163.com>
2025-04-18 10:46:19 +0800

    build(server): pm2日志调整为秒级

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       exec_mode: 'cluster', // 应用程序启动模式，这里设置的是cluster_mode（集群），默认是fork
       max_restarts: 10, // 设置应用程序异常退出重启的次数，默认15次（从0开始计数）
       restart_delay: 5000, // 异常重启情况下，延时重启时间
-      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       combine_logs: true,
       log_file: './logs/combined.outerr.log', // 日志目录
       out_file: './logs/out.log',
